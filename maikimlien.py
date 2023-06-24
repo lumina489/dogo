82e6bef45545dddcd8b21256ca98b9a27c3c9410
lumina489 <136431537+lumina489@users.noreply.github.com>
2023-06-24 19:18:27 +0700

    Create maikimlien.py

---
+++ maikimlien.py
@@ -0,0 +1,8 @@
+import numpy
+echo "# dogo" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/lumina489/dogo.git
+git push -u origin main
